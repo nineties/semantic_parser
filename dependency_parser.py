68452900ffd4760fadbd638a7e8e242e8fde396f
Koichi Nakamura <koichi@idein.jp>
2016-06-22 22:07:47 +0900

    Wrote DependencyParserServer.

--- dependency_parser.py
+++ dependency_parser.py
@@ -1,18 +1,31 @@
-from nltk.parse.stanford import StanfordDependencyParser
+import subprocess
 
-class StanfordDP(object):
-    def __init__(self, parser_path, models_path):
-        self.parser_path = parser_path
-        self.models_path = models_path
-        self.parser = StanfordDependencyParser(
-                path_to_jar=self.parser_path,
-                path_to_models_jar=self.models_path
-                )
+class DependencyParserServer(object):
+    def __init__(self, corenlp_path, port=9000, timeout=5000):
+        self.corenlp_path = corenlp_path
+        self.port = port
+        self.timeout = timeout
+        self.proc = None
 
-    def parse(self, text):
-        return self.parser.raw_parse(text).next()
+    def start(self):
+        if self.proc:
+            raise Exception('Double launch of dependency server.')
 
-    def parse_sents(self, texts):
-        result = self.parser.raw_parse_sents(texts)
-        for graph in result:
-            yield graph.next()
+        self.proc = subprocess.Popen([
+            'java', '-mx4g', '-cp', '{}/*'.format(self.corenlp_path),
+            'edu.stanford.nlp.pipeline.StanfordCoreNLPServer',
+            '-port', str(self.port),
+            '-timeout', str(self.timeout)
+            ],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+            )
+
+    def stop(self):
+        if self.proc:
+            self.proc.terminate()
+            self.proc = None
+
+    def __del__(self):
+        if self.proc:
+            self.proc.terminate()
