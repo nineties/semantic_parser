0df063319de981060508ad356ce876b47814e6a6
Koichi Nakamura <koichi@idein.jp>
2016-06-23 00:07:27 +0900

    Wrote DependencyParserClient.

--- dependency_parser.py
+++ dependency_parser.py
@@ -1,4 +1,6 @@
+import pandas as pd
 import subprocess
+import requests
 
 class DependencyParserServer(object):
     def __init__(self, corenlp_path, port=9000, timeout=5000):
@@ -35,3 +37,34 @@ class DependencyParserServer(object):
     def __del__(self):
         if self.proc:
             self.proc.terminate()
+
+class DependencyParserClient(object):
+    def __init__(self, host, port=9000):
+        self.host = host
+        self.port = port
+
+    def parse(self, text):
+        params = {
+            'annotators': 'parse',
+            'outputFormat': 'json'
+            }
+
+        data = requests.post(
+                'http://{}:{}'.format(self.host, self.port),
+                data=text,
+                params={'properties': repr(params)}
+                ).json()
+
+        # Convert JSON-object to panda's dataframes.
+        for s in data['sentences']:
+            df = pd.DataFrame.from_dict(s['tokens'])
+
+            original_text = ' '.join(df['originalText'])
+            tokens = df[['index', 'pos', 'word']]
+
+            df = pd.DataFrame.from_dict(s['basic-dependencies'])
+            print(df)
+            deps = df[['dep', 'governor', 'dependent']]
+            deps.columns = ['dep', 'lhs', 'rhs']
+
+            yield original_text, tokens, deps
