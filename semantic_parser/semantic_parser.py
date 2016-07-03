3dc15750f8356b66c74ab9b3f982f8c1737e83a3
Koichi Nakamura <koichi@idein.jp>
2016-07-03 19:26:48 +0900

    Wrote SemanticParser._create_initials .

--- semantic_parser/semantic_parser.py
+++ semantic_parser/semantic_parser.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from .dependency import DependencyParserClient
 
 class QuasiLogicalForm(object):
@@ -28,3 +29,33 @@ class SemanticParser(object):
             for text in corpus
             for tree in self.dpcli.parse(text)
             ]
+
+    def train(self, corpus, preprocess=True):
+        qlfs = self.preprocess(corpus) if preprocess else corpus
+        core_forms, arg_forms = self._create_initials(qlfs)
+
+    def _create_initials(self, qlfs):
+        # Re-index core forms so that they become unique.
+        offset = 0
+        for qlf in qlfs:
+            qlf.words.index += offset
+            qlf.deps['governor'] += offset
+            qlf.deps['dependent'] += offset
+            offset += len(qlf.words)
+
+        # Combine all tables.
+        core_forms = pd.concat(qlf.words for qlf in qlfs)
+        arg_forms = pd.concat(qlf.deps for qlf in qlfs)
+
+        # Create initial parts and clusters for each core forms.
+        core_forms['part'] = range(len(core_forms))
+        core_forms['cluster'] = range(len(core_forms))
+
+        # Assign arg forms to the initial parts.
+        arg_forms = pd.merge(arg_forms, core_forms[['part']],
+                left_on='governor', right_index=True)
+
+        # Create initial argtypes for each arg forms.
+        arg_forms['type'] = range(len(arg_forms))
+
+        return core_forms, arg_forms
